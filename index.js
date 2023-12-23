81c1999f1b99bde230088012a0588048c09cd910
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-12-23 10:13:43 +0500

    Create reducer and initialState

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const CAKE_ORDER = "CAKE_ORDER";
+const CAKE_ORDER = 'CAKE_ORDER';
 
 function orderCake() {
- return {
+  return {
     type: CAKE_ORDER,
     quantity: 1,
-  }
+  };
 }
+
+// (previousState, action) => newState
+
+const initialState = {
+  numOfCakes: 10,
+  anotherPropery: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDER:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
